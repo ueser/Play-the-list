8724f77a64e27b2117df37483a1b0a8470d1ccba
Umut Eser <umuteser@gmail.com>
2018-07-12 12:12:12 -0400

    add sample code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Play-the-list
 Toy project that allows me to listen to custom playlists
+
+```
+bash play_the_list.sh mood1.pls
+```
--- play_the_list.sh
+++ play_the_list.sh
@@ -6,16 +6,12 @@ num_lines=$(wc -l < "$1")
 echo ${num_lines}
 for i in $(seq 1 ${num_lines});
 do
-	echo "processing: $i"
 	idx=$((RANDOM % ${num_lines}))
-	echo "the idx is ${idx}"
 	
 	IFS=';' read -ra ADDR <<< "${playlist[idx]}"
 	name=${ADDR[0]}
-	echo "name is ${name}"
 
 	link=${ADDR[1]}
-	echo "link is ${link}"	
 
 	echo "Playing ${name} ..."
 	cvlc --no-video --play-and-exit ${link}
